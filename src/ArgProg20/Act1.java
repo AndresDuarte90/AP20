9fd057bf62e4eb3e66b9414134a58512767132ab
Andres Duarte <wolvyreyes@gmail.com>
2023-11-15 01:00:47 -0300

    Actividad 1 finalizada

--- src/ArgProg20/Act1.java
+++ src/ArgProg20/Act1.java
@@ -1,2 +1,71 @@
-package ArgProg20;public class Act1 {
+package ArgProg20;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Act1 {
+
+
+    static class Materia {
+        private String nombre;
+        private Set<Materia> correlativas;
+
+        public Materia(String nombre) {
+            this.nombre = nombre;
+            this.correlativas = new HashSet<>();
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public Set<Materia> getCorrelativas() {
+            return correlativas;
+        }
+    }
+
+    static class Alumno {
+        private String nombre;
+        private Set<Materia> materiasAprobadas;
+
+        public Alumno(String nombre) {
+            this.nombre = nombre;
+            this.materiasAprobadas = new HashSet<>();
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void aprobarMateria(Materia materia) {
+            materiasAprobadas.add(materia);
+        }
+
+        public Set<Materia> getMateriasAprobadas() {
+            return materiasAprobadas;
+        }
+    }
+
+    static class Inscripcion {
+        private Alumno alumno;
+        private Materia materia;
+
+        public Inscripcion(Alumno alumno, Materia materia) {
+            this.alumno = alumno;
+            this.materia = materia;
+        }
+
+        public boolean aprobada() {
+            return alumno.getMateriasAprobadas().containsAll(materia.getCorrelativas());
+        }
+
+        @Override
+        public String toString() {
+            return "Inscripción: " + alumno.getNombre() + " en " + materia.getNombre();
+        }
+    }
+
+
+
+
 }
---
+++ src/ArgProg20/Main.java
@@ -0,0 +1,27 @@
+package ArgProg20;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+                Act1.Materia algoritmos = new Act1.Materia("Algoritmos y Estructuras de Datos");
+                Act1.Materia paradigmas = new Act1.Materia("Paradigmas de Programación");
+                Act1.Materia disenoSistemas = new Act1.Materia("Diseño de Sistemas");
+
+                paradigmas.getCorrelativas().add(algoritmos);
+                disenoSistemas.getCorrelativas().add(paradigmas);
+
+                Act1.Alumno alumno1 = new Act1.Alumno("Andres");
+                alumno1.aprobarMateria(algoritmos);
+                alumno1.aprobarMateria(paradigmas);
+
+                Act1.Alumno alumno2 = new Act1.Alumno("Laura");
+                alumno2.aprobarMateria(algoritmos);
+
+                Act1.Inscripcion inscripcion1 = new Act1.Inscripcion(alumno1, disenoSistemas);
+                Act1.Inscripcion inscripcion2 = new Act1.Inscripcion(alumno2, disenoSistemas);
+
+                System.out.println(inscripcion1 + " - Aprobada: " + inscripcion1.aprobada());
+                System.out.println(inscripcion2 + " - Aprobada: " + inscripcion2.aprobada());
+    }
+}
